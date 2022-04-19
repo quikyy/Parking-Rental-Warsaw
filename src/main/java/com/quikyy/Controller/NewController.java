a0c306c0ff98d73c949a1f365a297016aafd54fe
quikyy <paczuskiszymon16@gmail.com>
2022-04-19 23:23:28 +0200

    19042022

--- src/main/java/com/quikyy/Controller/NewController.java
+++ src/main/java/com/quikyy/Controller/NewController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
